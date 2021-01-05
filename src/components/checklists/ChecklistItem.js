b4835028c4b4f82314db446bda1e4c658c017281
samanthajmaas <samanthajmaas@gmail.comcdclear>
2021-01-05 13:38:00 -0600

    checklist and budget styling

--- src/components/checklists/ChecklistItem.js
+++ src/components/checklists/ChecklistItem.js
@@ -15,53 +15,74 @@ export const ChecklistItem = (props) => {
     return (
         <>
             <section className="checklistItem">
-                <>
-                    <div>
-                        {
-                            props.item.completed_date != null ?
-                                <>
-                                    <div className="top-item-cont">
-                                        <Checkbox
-                                            checked="true"
-                                            onChange={handleChange}
-                                            color="#996D70"
-                                            inputProps={{ 'aria-label': 'primary checkbox' }}
-                                        />
-                                        <div className="item-name" style={{ textDecorationLine: 'line-through' }}>{props.item.checklist_item.toDo}</div>
-                                        {
-                                            props.editMode ?
-
-                                                <button className="sml-btn" onClick={() => deleteChecklistItem(props.item)}> delete item</button> 
-                                                :
-                                                null
-                                        }
-                                    </div>
-                                    <div className="completed">Completed on: {new Date(props.item.completed_date.concat("T00:00:00")).toDateString({})}</div>
-
-                                </>
-
-                                :
-                                <>
-                                    <div className="item-cont">
-                                        <Checkbox
-                                            checked={checked}
-                                            onChange={handleChange}
-                                            color="#996D70"
-                                            inputProps={{ 'aria-label': 'primary checkbox' }}
-                                        />
-                                        <div className="item-name">{props.item.checklist_item.toDo}</div>
-                                        {
-                                            props.editMode ?
-
-                                                <button className="sml-btn" onClick={() => deleteChecklistItem(props.item)}> delete item</button> 
-                                                :
-                                                null
-                                        }
-                                    </div>
-                                </>
-                        }
-                    </div>
-                </>
+                <div>
+                    {
+                        props.item.completed_date != null ?
+                            <>
+                                {props.editMode ?
+                                    <>
+                                        <div className="edit-checklist-container">
+                                            <div className="top-item-cont">
+                                                <button className="btn delete-check-btn" onClick={() => deleteChecklistItem(props.item)}> delete </button>
+                                                <Checkbox
+                                                    checked="true"
+                                                    onChange={handleChange}
+                                                    color="#996D70"
+                                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                                />
+                                                <div className="edit-item-name" style={{ textDecorationLine: 'line-through' }}>{props.item.checklist_item.toDo}</div>
+                                            </div>
+                                            <div className="edit-completed">Completed on: {new Date(props.item.completed_date.concat("T00:00:00")).toDateString({})}</div>
+                                        </div>
+                                    </> :
+                                    <>
+                                        <div className="top-item-cont">
+                                            <Checkbox
+                                                checked="true"
+                                                onChange={handleChange}
+                                                color="#996D70"
+                                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                                            />
+                                            <div className="item-name" style={{ textDecorationLine: 'line-through' }}>{props.item.checklist_item.toDo}</div>
+                                        </div>
+                                        <div className="completed">Completed on: {new Date(props.item.completed_date.concat("T00:00:00")).toDateString({})}</div>
+                                    </>
+                                }
+                            </>
+                            :
+                            <>
+                                <div className="item-cont">
+                                    {
+                                        props.editMode ?
+                                            <>
+                                                <div className="edit-checklist-container">
+                                                    <div className="top-item-cont">
+                                                        <button className="btn delete-check-btn" onClick={() => deleteChecklistItem(props.item)}> delete </button>
+                                                        <Checkbox
+                                                            checked={checked}
+                                                            onChange={handleChange}
+                                                            color="#996D70"
+                                                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                                                        />
+                                                        <div className="edit-item-name">{props.item.checklist_item.toDo}</div>
+                                                    </div>
+                                                </div>
+                                            </>
+                                            :
+                                            <>
+                                                <Checkbox
+                                                    checked={checked}
+                                                    onChange={handleChange}
+                                                    color="#996D70"
+                                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                                />
+                                                <div className="item-name">{props.item.checklist_item.toDo}</div>
+                                            </>
+                                    }
+                                </div>
+                            </>
+                    }
+                </div>
             </section>
         </>
     )
